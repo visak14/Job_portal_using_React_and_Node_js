6facc39f68b566e12a7c89be778f0bb25e4d731a
visak14 <vishalkesharwani1406@gmail.com>
2023-10-22 02:19:12 +0530

    port change after deploy

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const options = {
         },
         servers :[ 
             {
-            url:"http://localhost:8081"
+            url:"https://job-portal-using-react-and-node-js.onrender.com"
         }
     ]
     },
